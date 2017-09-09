2653d1ceb6e1de94428fad8993d5e16c761c44a7
Evan Blanke <evanblanke@gmail.com>
2017-09-09 13:46:28 -0500

    Added last years teleop and harware configuration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AManualD.java
@@ -0,0 +1,180 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "Linear Driver TeleOp", group = "TeleOp")
+
+public class AManualD extends LinearOpMode {
+    Hardware robot = new Hardware();
+    private int collectState = 0, liftState = 0, shootCount = 0, rpm = 2550;
+    private boolean shootTog = false, spinTog = false;
+
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        robot.cannonMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("Say", "Good morning");
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            control();
+        }
+    }
+
+    public void control() throws java.lang.InterruptedException {
+        telem();
+        drive();
+        collect();
+        lift();
+        shoot();
+        robot.waitForTick(40);
+    }
+
+    private void telem() {
+        telemetry.clear();
+        telemetry.addData("WidowMaker", ((shootTog) ? "On" : "Off") + " rpm: " + rpm);
+        telemetry.addData("Lift", (liftState == 1) ? "Up" : (liftState == -1) ? "Down" : "Off");
+        telemetry.addData("LeftStick", gamepad1.left_stick_y);
+        telemetry.addData("RightStick", gamepad1.right_stick_y);
+        telemetry.update();
+    }
+
+    private void drive() throws NullPointerException {
+        double leftStickVert = Range.clip(gamepad1.left_stick_y, -1.0, 1.0);
+        double rightStickVert = Range.clip(gamepad1.right_stick_y, -1.0, 1.0);
+
+        joyDir(leftStickVert, rightStickVert);
+
+        leftStickVert = Math.abs(leftStickVert);
+        rightStickVert = Math.abs(rightStickVert);
+
+        if (leftStickVert <= .025 && rightStickVert <= .025)
+            dPad();
+        else {
+            motorPow(leftStickVert, rightStickVert);
+        }
+    }
+
+    private String joyDir(double leftStickVert, double rightStickVert) {
+        String joyDirTel = "";
+        if (leftStickVert > 0) {
+            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            joyDirTel += "Left Forward ";
+        } else if (leftStickVert < 0) {
+            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            joyDirTel += "Left Backwards ";
+        }
+        if (rightStickVert > 0) {
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            joyDirTel += "Right Forward ";
+        } else if (rightStickVert < 0) {
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            joyDirTel += "Right Backwards ";
+        }
+        return joyDirTel;
+    }
+
+    private void dPad() {
+        if (gamepad1.dpad_up) {
+            telemetry.addData("Say", "Forward");
+            telemetry.update();
+
+            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            robot.frontLeftMotor.setPower(.5);
+            robot.backLeftMotor.setPower(.5);
+            robot.frontRightMotor.setPower(.5);
+            robot.backRightMotor.setPower(.5);
+        } else if (gamepad1.dpad_down) {
+            telemetry.addData("Say", "Backward");
+            telemetry.update();
+
+            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+            robot.frontLeftMotor.setPower(.5);
+            robot.backLeftMotor.setPower(.5);
+            robot.frontRightMotor.setPower(.5);
+            robot.backRightMotor.setPower(.5);
+        } else {
+            robot.frontLeftMotor.setPower(0);
+            robot.backLeftMotor.setPower(0);
+            robot.frontRightMotor.setPower(0);
+            robot.backRightMotor.setPower(0);
+        }
+    }
+
+    private void motorPow(double leftStickVert, double rightStickVert) {
+        robot.frontLeftMotor.setPower(leftStickVert);
+        robot.backLeftMotor.setPower(leftStickVert);
+        robot.frontRightMotor.setPower(rightStickVert);
+        robot.backRightMotor.setPower(rightStickVert);
+    }
+
+    private void collect() {
+
+        if (gamepad1.x)
+            rpm = 3000;
+        if (gamepad1.y)
+            rpm = 2650;
+        if (gamepad1.b)
+            rpm = 2550;
+        if (gamepad1.a)
+            rpm = 2000;
+
+        if (gamepad1.right_trigger > .1)
+            rpm-=10;
+        else if (gamepad1.left_trigger > .1)
+            rpm+=10;
+
+        if (spinTog && gamepad2.b) {
+            spinTog = false;
+        } else if (!gamepad2.b)
+            spinTog = true;
+    }
+
+    private void lift() {
+        if (gamepad2.a)
+            liftState = 0;
+        else if (gamepad2.right_trigger != 0)
+            liftState = 1;
+        else if (gamepad2.left_trigger != 0)
+            liftState = -1;
+
+        if (liftState == 1)
+            robot.lift.setPower(1.0);
+        else if (liftState == -1)
+            robot.lift.setPower(-1.0);
+        else
+            robot.lift.setPower(0);
+    }
+
+    private void shoot() {
+        shootCount--;
+
+        if (gamepad2.x && shootCount < 0) {
+            shootCount = 80;
+            shootTog = !shootTog;
+        }
+
+        if (shootTog) {
+            robot.cannonMotor.setPower(.35);
+        } else
+            robot.cannonMotor.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,136 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+public class Hardware{
+    DcMotor frontLeftMotor = null;
+    DcMotor frontRightMotor = null;
+    DcMotor backLeftMotor = null;
+    DcMotor backRightMotor = null;
+    DcMotor cannonMotor = null;
+    DcMotor lift = null;
+
+    ColorSensor beaconSensor = null;
+    ModernRoboticsI2cGyro gyro = null;
+
+    VuforiaLocalizer vuforia = null;
+    VuforiaTrackables beacons = null;
+
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+    static final int tickSpeed = 1120, distanceFromCentermm = 203;
+    static final double wheelCircumferencemm = 159.5929;
+
+
+
+
+    public Hardware() {
+    }
+
+    public void init(HardwareMap ahwMap) throws InterruptedException{
+        hwMap = ahwMap;
+
+        InitComponents();
+        InitVuforia();
+    }
+
+
+    private void InitVuforia() {
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        params.vuforiaLicenseKey = "AfmBbcz/////AAAAGbLGg++zzk4MiOrcPTc3t9xQj3QHfISJprebOgt5JJ4+83xtFO+ApGlI3GVY/aMgCpoGEIzaJse9sXiYDiLYpJQlGDX765tWJUrqM+pzqLxVXjWA1J6c968/YqYq74Vq5emNxGHj5SF3HP3m43Iq/YYgkSdMv4BR+RThPPnIIzrbAjEAHHtMgH7vVh036+bcw9UqBfSdD/IBqrKpJLERn5+Qi/4Q4EoReCC0CTDfZ+LcY0rUur0QZRkMpxx/9s4eCgIU+qfOcSlBvjoX7QAQ2MImUME1y5yJiyaWueamnhRBOwERGBuDKyGp4eBWp4i3esJcplrWYovjzPg9fL7Thy8v9KnrHy22PUFAYY+1vjKp";
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
+        vuforia = ClassFactory.createVuforiaLocalizer(params);
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+
+        beacons.get(0).setName("Wheels");
+        beacons.get(1).setName("Tools");
+        beacons.get(2).setName("Lego");
+        beacons.get(3).setName("Gears");
+    }
+
+    private void InitComponents() {
+
+        GetMotors();
+
+        InitMotorsDirection();
+
+        SetMotorsInitPower();
+
+        SetMotorInitMode();
+
+        InitSensors();
+
+        cannonMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    private void InitSensors(){
+        beaconSensor = hwMap.colorSensor.get("beacon");
+        beaconSensor.enableLed(false);
+
+        gyro = (ModernRoboticsI2cGyro)hwMap.gyroSensor.get("gyro");
+
+        gyro.calibrate();
+
+    }
+
+    private void SetMotorInitMode() {
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        cannonMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private void SetMotorsInitPower() {
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        cannonMotor.setPower(0);
+        lift.setPower(0);
+    }
+
+    private void InitMotorsDirection() {
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        cannonMotor.setDirection(DcMotor.Direction.FORWARD);
+        lift.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    private void GetMotors() {
+        frontLeftMotor = hwMap.dcMotor.get("frontLeftMotor");
+        frontRightMotor = hwMap.dcMotor.get("frontRightMotor");
+        backLeftMotor = hwMap.dcMotor.get("backLeftMotor");
+        backRightMotor = hwMap.dcMotor.get("backRightMotor");
+        cannonMotor = hwMap.dcMotor.get("cannonMotor");
+        lift = hwMap.dcMotor.get("lift");
+    }
+
+    public void waitForTick(long periodMs) throws InterruptedException {
+
+        long remaining = periodMs - (long) period.milliseconds();
+
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        period.reset();
+    }
+}
