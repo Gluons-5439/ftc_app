b8d3ecb5cf74f77a9cb2864efedfd195a1976f62
Evan Blanke <evanblanke@gmail.com>
2017-09-16 00:57:03 -0500

    Whittled down to 31 lines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AManualD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AManualD.java
@@ -2,53 +2,30 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "Linear Driver TeleOp", group = "TeleOp")
 
 public class AManualD extends LinearOpMode {
     Hardware robot = new Hardware();
-
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
-
         waitForStart();
-
         while (opModeIsActive()) {
-            control();
+            if(gamepad1.left_bumper) throw new NullPointerException();
+
+            robot.frontLeftMotor.setPower(gamepad1.left_stick_y);
+            robot.backLeftMotor.setPower(gamepad1.left_stick_y);
+            robot.frontRightMotor.setPower(gamepad1.right_stick_y);
+            robot.backRightMotor.setPower(gamepad1.right_stick_y);
+
+            if(gamepad1.right_bumper || gamepad2.right_bumper) robot.cannonMotor.setPower(1);
+            else robot.cannonMotor.setPower(0);
+
+            if (gamepad1.right_trigger > 0.1 || gamepad2.right_trigger > 0.1) robot.lift.setPower(1.0);
+            else if (gamepad1.left_trigger > 0.1 || gamepad2.left_trigger > 0.1) robot.lift.setPower(-1.0);
+            else robot.lift.setPower(0);
+
+            robot.waitForTick(40);
         }
-    }
-
-    public void control() throws java.lang.InterruptedException {
-        double left = Range.clip(gamepad1.left_stick_y, -1.0, 1.0);
-        double right = Range.clip(gamepad1.right_stick_y, -1.0, 1.0);
-
-        if(gamepad1.left_bumper){
-            throw new NullPointerException();
-        }
-
-        motorPow(left, right);
-
-        if(gamepad1.right_bumper || gamepad2.right_bumper) robot.cannonMotor.setPower(1);
-        else robot.cannonMotor.setPower(0);
-
-        if (gamepad1.right_trigger > 0 || gamepad2.right_trigger > 0)
-            robot.lift.setPower(1.0);
-        else if (gamepad1.left_trigger > 0 || gamepad2.left_trigger > 0)
-            robot.lift.setPower(-1.0);
-        else
-            robot.lift.setPower(0);
-
-        robot.waitForTick(40);
-    }
-
-    private void motorPow(double left, double right) {
-        robot.frontLeftMotor.setPower(left);
-        robot.backLeftMotor.setPower(left);
-        robot.frontRightMotor.setPower(right);
-        robot.backRightMotor.setPower(right);
     }
 }
