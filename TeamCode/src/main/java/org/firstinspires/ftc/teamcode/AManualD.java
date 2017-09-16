7c1d133db2795fdd115636a581542747aaaeb509
Evan Blanke <evanblanke@gmail.com>
2017-09-16 00:17:49 -0500

    Fixed Teleop for taste of St. Louis... Again... But wait its actually that day you wouldnt think we would keep changing stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AManualD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AManualD.java
@@ -11,170 +11,57 @@ import com.qualcomm.robotcore.util.Range;
 
 public class AManualD extends LinearOpMode {
     Hardware robot = new Hardware();
-    private int collectState = 0, liftState = 0, shootCount = 0, rpm = 2550;
-    private boolean shootTog = false, spinTog = false;
 
     public void runOpMode() throws InterruptedException {
-
         robot.init(hardwareMap);
 
-        robot.cannonMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        telemetry.addData("Say", "Good morning");
-        telemetry.update();
         waitForStart();
+
         while (opModeIsActive()) {
             control();
         }
     }
 
     public void control() throws java.lang.InterruptedException {
-        telem();
-        drive();
-        collect();
-        lift();
-        shoot();
-        robot.waitForTick(40);
-    }
+        boolean shootTog = false, spinTog = false;
 
-    private void telem() {
-        telemetry.clear();
-        telemetry.addData("WidowMaker", ((shootTog) ? "On" : "Off") + " rpm: " + rpm);
-        telemetry.addData("Lift", (liftState == 1) ? "Up" : (liftState == -1) ? "Down" : "Off");
-        telemetry.addData("LeftStick", gamepad1.left_stick_y);
-        telemetry.addData("RightStick", gamepad1.right_stick_y);
-        telemetry.update();
-    }
+        double left = Range.clip(gamepad1.left_stick_y, -1.0, 1.0);
+        double right = Range.clip(gamepad1.right_stick_y, -1.0, 1.0);
 
-    private void drive() throws NullPointerException {
-        double leftStickVert = Range.clip(gamepad1.left_stick_y, -1.0, 1.0);
-        double rightStickVert = Range.clip(gamepad1.right_stick_y, -1.0, 1.0);
-
-        joyDir(leftStickVert, rightStickVert);
-
-        leftStickVert = Math.abs(leftStickVert);
-        rightStickVert = Math.abs(rightStickVert);
-
-        if (leftStickVert <= .025 && rightStickVert <= .025)
+        if (left <= .025 && right <= .025)
             dPad();
         else {
-            motorPow(-leftStickVert, -rightStickVert);
+            motorPow(-left, -right);
         }
-    }
 
-    private String joyDir(double leftStickVert, double rightStickVert) {
-        String joyDirTel = "";
-        if (leftStickVert > 0) {
-            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            joyDirTel += "Left Forward ";
-        } else if (leftStickVert < 0) {
-            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            joyDirTel += "Left Backwards ";
-        }
-        if (rightStickVert > 0) {
-            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            robot.backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            joyDirTel += "Right Forward ";
-        } else if (rightStickVert < 0) {
-            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            robot.backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            joyDirTel += "Right Backwards ";
-        }
-        return joyDirTel;
-    }
+        if(gamepad1.b) spinTog = spinTog ? false:true;
+        robot.cannonMotor.setPower(spinTog ? 1:0);
 
-    private void dPad() {
-        if (gamepad1.dpad_up) {
-            telemetry.addData("Say", "Forward");
-            telemetry.update();
+        if(gamepad1.right_bumper) shootTog = shootTog ? false:true;
+        robot.cannonMotor.setPower(shootTog ? 1:0);
 
-            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            robot.backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            robot.frontLeftMotor.setPower(.5);
-            robot.backLeftMotor.setPower(.5);
-            robot.frontRightMotor.setPower(.5);
-            robot.backRightMotor.setPower(.5);
-        } else if (gamepad1.dpad_down) {
-            telemetry.addData("Say", "Backward");
-            telemetry.update();
 
-            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            robot.backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-            robot.frontLeftMotor.setPower(.5);
-            robot.backLeftMotor.setPower(.5);
-            robot.frontRightMotor.setPower(.5);
-            robot.backRightMotor.setPower(.5);
-        } else {
-            robot.frontLeftMotor.setPower(0);
-            robot.backLeftMotor.setPower(0);
-            robot.frontRightMotor.setPower(0);
-            robot.backRightMotor.setPower(0);
-        }
-    }
-
-    private void motorPow(double leftStickVert, double rightStickVert) {
-        robot.frontLeftMotor.setPower(leftStickVert);
-        robot.backLeftMotor.setPower(leftStickVert);
-        robot.frontRightMotor.setPower(rightStickVert);
-        robot.backRightMotor.setPower(rightStickVert);
-    }
-
-    private void collect() {
-
-        if (gamepad1.x)
-            rpm = 3000;
-        if (gamepad1.y)
-            rpm = 2650;
-        if (gamepad1.b)
-            rpm = 2550;
-        if (gamepad1.a)
-            rpm = 2000;
-
-        if (gamepad1.right_trigger > .1)
-            rpm-=10;
-        else if (gamepad1.left_trigger > .1)
-            rpm+=10;
-
-        if (spinTog && gamepad2.b) {
-            spinTog = false;
-        } else if (!gamepad2.b)
-            spinTog = true;
-    }
-
-    private void lift() {
-        if (gamepad2.a)
-            liftState = 0;
-        else if (gamepad2.right_trigger != 0)
-            liftState = 1;
-        else if (gamepad2.left_trigger != 0)
-            liftState = -1;
-
-        if (liftState == 1)
+        if (gamepad1.right_trigger > 0)
             robot.lift.setPower(1.0);
-        else if (liftState == -1)
+        else if (gamepad1.left_trigger > 0)
             robot.lift.setPower(-1.0);
         else
             robot.lift.setPower(0);
+
+        robot.waitForTick(40);
     }
 
-    private void shoot() {
-        shootCount--;
+    private void dPad() {
+        if (gamepad1.dpad_up) motorPow(0.5,0.5);
+        else if (gamepad1.dpad_down) motorPow(-0.5,-0.5);
+        else motorPow(0,0);
+    }
 
-        if (gamepad2.x && shootCount < 0) {
-            shootCount = 80;
-            shootTog = !shootTog;
-        }
-
-        if (shootTog) {
-            robot.cannonMotor.setPower(1);
-        } else
-            robot.cannonMotor.setPower(0);
+    private void motorPow(double left, double right) {
+        robot.frontLeftMotor.setPower(left);
+        robot.backLeftMotor.setPower(left);
+        robot.frontRightMotor.setPower(right);
+        robot.backRightMotor.setPower(right);
     }
 }
