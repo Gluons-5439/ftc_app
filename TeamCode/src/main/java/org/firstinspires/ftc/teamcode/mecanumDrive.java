d3cd2724fd0624069fea0d4c1c04108fdc24fdd4
Evan Blanke <evanblanke@gmail.com>
2017-10-10 18:31:05 -0500

    Added mecanum drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "MecanumDrive", group = "TeleOp")
+
+public class mecanumDrive extends LinearOpMode {
+    Hardware robot = new Hardware();
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            float forward = -gamepad1.left_stick_y;
+            float right = gamepad1.left_stick_x;
+            float clockwise = gamepad1.right_stick_x;
+
+            robot.frontLeftMotor.setPower(Range.clip(forward+clockwise+right,-1,1));
+            robot.backLeftMotor.setPower(Range.clip(forward-clockwise-right,-1,1));
+            robot.frontRightMotor.setPower(Range.clip(forward+clockwise-right,-1,1));
+            robot.backRightMotor.setPower(Range.clip(forward-clockwise+right,-1,1));
+
+            robot.waitForTick(40);
+        }
+    }
+}
