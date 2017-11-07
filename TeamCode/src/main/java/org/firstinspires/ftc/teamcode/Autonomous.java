54053b399df27cb69d568599e40ecd060b00b137
akhorana14 <akhorana14@yahoo.com>
2017-11-07 17:00:36 -0600

    Added methods to go forwards back left and right. First need to determine servo measurements in comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -17,45 +17,41 @@ public class Autonomous extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
 
-        while (opModeIsActive()) {
-            double theta = gyro.getHeading();
-            //Creates variable theta which equals robot heading
+        //Need to first put jewel servo down between balls to sense it. Then turn right/left depending on which one and back, go forward and place block into cryptobox
 
-            //failsafe: switch forward and right
-            double forward = Math.abs(gamepad1.left_stick_y) > 0.1 ? -gamepad1.left_stick_y : 0;
-            double right = Math.abs(gamepad1.left_stick_x) > 0.1 ? gamepad1.left_stick_x : 0;
-            double clockwise = Math.abs(gamepad1.right_stick_x) > 0.1 ? gamepad1.right_stick_x : 0;
-            //Variables to set values based on controller input, 0.1 is deadzone
-
-            double temp = forward * Math.cos(Math.toRadians(theta)) - right * Math.sin(Math.toRadians(theta));
-            right = forward * Math.sin(theta) + right * Math.cos((theta));
-            forward = temp;
-            //Math for drive relative to theta
-
-            clockwise *= -0.5;
-            //Sets speed when rotating, still needs work
-
-            robot.frontLeftMotor.setPower(Range.clip(forward + clockwise + right, -1, 1));
-            robot.backLeftMotor.setPower(Range.clip(forward + clockwise - right, -1, 1));
-            robot.frontRightMotor.setPower(Range.clip(forward - clockwise - right, -1, 1));
-            robot.backRightMotor.setPower(Range.clip(forward - clockwise + right, -1, 1));
-            //Three linear variables intersecting non-linearly for mecanum drive
-
-            float padTwoLeftY = Math.abs(gamepad2.left_stick_y) > 0.2 ? -gamepad2.left_stick_y : 0;
-            //Deadzone for lift motors
-            robot.liftMotorLeft.setPower(padTwoLeftY);
-            robot.liftMotorRight.setPower(padTwoLeftY);
-            //Sets power for motors raising and lowering pulley equal to gamepad2 left stick
-
-            telemetry.addData("Heading of Gyro:", theta);
-            telemetry.update();
-            //Adds gyro heading to telemetry
-
-            robot.waitForTick(40);
-            //Stops phone from queuing too many commands and breaking
-
-        }
     }
 
+    public void goForward() {
+
+        frontLeftMotor.setPower(1);
+        frontRightMotor.setPower(1);
+        backLeftMotor.setPower(1);
+        backRightMotor.setPower(1);
+    }
+
+    public void goBackward() {
+
+        frontLeftMotor.setPower(-1);
+        frontRightMotor.setPower(-1);
+        backLeftMotor.setPower(-1);
+        backRightMotor.setPower(-1);
+
+    }
+
+    public void goRight() {
+        frontLeftMotor.setPower(-1);
+        frontRightMotor.setPower(1);
+        backLeftMotor.setPower(-1);
+        backRightMotor.setPower(1);
+
+    }
+
+    public void goLeft() {
+        frontLeftMotor.setPower(1);
+        frontRightMotor.setPower(-1);
+        backLeftMotor.setPower(1);
+        backRightMotor.setPower(-1);
+
+    }
 
 }
